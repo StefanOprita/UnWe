f9aa22cf2a5fb81b0a54a54ff65fa34fb6555102
Stefan Oprita <oprita.stefan@yahoo.com>
2021-04-08 15:35:12 +0300

    Worked on statistics box

--- app/views/statistics/script.js
+++ app/views/statistics/script.js
@@ -11,12 +11,62 @@ var CountyOnClickFunctions= new Array;
 var CountyOnMouseEnterFunctions= new Array;
 var CountyOnMouseLeaveFunctions = new Array;
 
+var listOfChartButtons = document.getElementsByClassName('type-of-chart-selector-item');
+
+
+// let countyColors = [
+//     '#F44336', '#9C27B0', '#2196F3', '#009688', '#FFEB3B', '#795548',
+//     '#E91E63', '#673AB7', '#3F51B5', '#03A9F4', '#00BCD4', '#4CAF50',
+//     '#8BC34A', '#CDDC39', '#FFC107', '#FF9800', '#FF5722', '#9E9E9E',
+//     '#000'
+// ];
+
 let countyColors = [
-    '#F44336', '#9C27B0', '#2196F3', '#009688', '#FFEB3B', '#795548',
-    '#E91E63', '#673AB7', '#3F51B5', '#03A9F4', '#00BCD4', '#4CAF50',
-    '#8BC34A', '#CDDC39', '#FFC107', '#FF9800', '#FF5722', '#9E9E9E',
-    '#000'
-];
+    '#40db71',
+    '#fc4a26',
+    '#fcc726',
+    '#8ce322',
+    '#002e0c',
+    '#2e0800',
+    '#67cfc8',
+    '#6847a6',
+    '#f7639c',
+    '#50ad78',
+    '#2a2252',
+    '#50ad50',
+    '#f7825c',
+    '#9bf244',
+    '#ca3c25',
+    '#008148',
+    '#ef8a17',
+    '#a882dd',
+    '#2d93ad',
+    '#a62375',
+    '#7272ab',
+    '#53b3cb',
+    '#2191fb',
+    '#b7b868',
+    '#8fd694',
+    '#4f5d75',
+    '#f5cd5f',
+    '#bfc0c0',
+    '#2d3142',
+    '#6247aa',
+    '#a06cd5',
+    '#ff88dc',
+    '#91a6ff',
+    '#c46590',
+    '#bf3232',
+    '#c897ee',
+    '#c24c61',
+    '#c67ebf',
+    '#da4473',
+    '#ffd942',
+    '#2d4200',
+    '#750d37',
+    '#19aabd',
+    '#ff666d'
+]
 
 let availableCountyColors = countyColors;
 
@@ -507,6 +557,45 @@ function closeMap() {
     const button = document.getElementsByClassName('map-button')[0];
     button.style.opacity = 1;
 
+}
+
+function chartButtonClicked(element) {
+    console.log(element);
+    for (let index = 0; index < listOfChartButtons.length; index++) {
+        const ele = listOfChartButtons[index];
+        console.log(ele)
+        if(ele === element) {
+            ele.classList.add('type-of-chart-selector-item-selected');
+        } else {
+            ele.classList.remove('type-of-chart-selector-item-selected');
+        }
+        
+    }
+
+}
+
+
+function changeCurrentChartCategory(e)
+{
+    const element = document.getElementsByClassName('chart-options-menu')[0]
+                    .getElementsByClassName('top-bar')[0]
+                    .getElementsByClassName('text')[0];
+
+    element.innerHTML = e.innerHTML;
+    clickChartCategorySelector();
+}
+
+function clickChartCategorySelector()
+{
+    const options = document.getElementsByClassName('chart-options-menu-selector')[0];
+    console.log(options.classList);
+    if(options.classList.contains('chart-options-menu-selector-closed')) {
+        options.classList.remove('chart-options-menu-selector-closed');
+        options.classList.add('chart-options-menu-selector-opened');
+    } else {
+        options.classList.add('chart-options-menu-selector-closed');
+        options.classList.remove('chart-options-menu-selector-opened');
+    }
 }
 
 addCountyFunction('onclick', changeColorOfCounty);
