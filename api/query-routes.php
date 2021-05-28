2916e0ebe24d2ad917523a0743de5d7d8d732f01
LiviuC <62299917+LiviuC-glitch@users.noreply.github.com>
2021-05-28 16:03:49 +0300

    json data from db

--- api/query-routes.php
+++ api/query-routes.php
@@ -16,55 +16,55 @@ $query_routes = [
 function jsonFormat($resultArray, $categs)
 {
     $toAdd = [];
-    $toAdd['name'] = $resultArray[0]['judet'];
-    $toAdd['id'] = Query::getIdCounty($resultArray[0]['judet']);
-    $toAdd['total'] = $resultArray[0]['total_someri'];
+    $toAdd['name'] = $resultArray['judet'];
+    $toAdd['id'] = Query::getIdCounty($resultArray['judet']);
+    $toAdd['total'] = $resultArray['total_someri'];
 
     if (in_array('gender', $categs, false) || $categs == []) {
         $genders = [];
-        $genders['male'] =  $resultArray[0]['total_barbati'];
-        $genders['female'] =  $resultArray[0]['total_femei'];
+        $genders['male'] =  $resultArray['total_barbati'];
+        $genders['female'] =  $resultArray['total_femei'];
         $toAdd['gender'] = $genders;
     }
 
     if (in_array('compensation', $categs, false) || $categs == []) {
         $comp = [];
-        $comp['compensated'] =  $resultArray[0]['indemnizati'];
-        $comp['non-compensated'] =  $resultArray[0]['neindemnizati'];
+        $comp['compensated'] =  $resultArray['indemnizati'];
+        $comp['non-compensated'] =  $resultArray['neindemnizati'];
         $toAdd['compensation'] = $comp;
     }
 
     if (in_array('education', $categs, false) || $categs == []) {
         $educ = [];
-        $educ['no education'] = $resultArray[0]['fara_studii'];
-        $educ['primary'] = $resultArray[0]['invatamant_primar'];
-        $educ['middleschool'] = $resultArray[0]['invatamant_gimnazial'];
-        $educ['highschool'] = $resultArray[0]['invatamant_liceal'];
-        $educ['post-highschool'] = $resultArray[0]['invatamant_post'];
-        $educ['professional'] = $resultArray[0]['invatamant_profesional'];
-        $educ['bachelors'] = $resultArray[0]['invatamant_universitar'];
+        $educ['no education'] = $resultArray['fara_studii'];
+        $educ['primary'] = $resultArray['invatamant_primar'];
+        $educ['middleschool'] = $resultArray['invatamant_gimnazial'];
+        $educ['highschool'] = $resultArray['invatamant_liceal'];
+        $educ['post-highschool'] = $resultArray['invatamant_post'];
+        $educ['professional'] = $resultArray['invatamant_profesional'];
+        $educ['bachelors'] = $resultArray['invatamant_universitar'];
         $toAdd['education'] = $educ;
     }
 
     if (in_array('rate', $categs, false) || $categs == []) {
         $rates = [];
-        $rates['total'] = $resultArray[0]['rata_somaj'];
-        $rates['male'] = $resultArray[0]['rata_somaj_barbati'];
-        $rates['female'] = $resultArray[0]['rata_somaj_femei'];
+        $rates['total'] = $resultArray['rata_somaj'];
+        $rates['male'] = $resultArray['rata_somaj_barbati'];
+        $rates['female'] = $resultArray['rata_somaj_femei'];
         $toAdd['rate'] = $rates;
     }
 
     if (in_array('environment', $categs, false) || $categs == []) {
         $envir = [];
         $urb = [];
-        $urb['total'] = $resultArray[0]['total_urban'];
-        $urb['male'] = $resultArray[0]['barbati_urban'];
-        $urb['female'] = $resultArray[0]['femei_urban'];
+        $urb['total'] = $resultArray['total_urban'];
+        $urb['male'] = $resultArray['barbati_urban'];
+        $urb['female'] = $resultArray['femei_urban'];
 
         $rur = [];
-        $rur['total'] = $resultArray[0]['total_rural'];
-        $rur['male'] = $resultArray[0]['barbati_rural'];
-        $rur['female'] = $resultArray[0]['femei_rural'];
+        $rur['total'] = $resultArray['total_rural'];
+        $rur['male'] = $resultArray['barbati_rural'];
+        $rur['female'] = $resultArray['femei_rural'];
 
         $envir['urban'] = $urb;
         $envir['rural'] = $rur;
@@ -74,19 +74,20 @@ function jsonFormat($resultArray, $categs)
 
     if (in_array('age', $categs, false) || $categs == []) {
         $ages = [];
-        $ages['<25'] = $resultArray[0]['sub_25'];
-        $ages['25-29'] = $resultArray[0]['25_29'];
-        $ages['30-39'] = $resultArray[0]['30_39'];
-        $ages['40-49'] = $resultArray[0]['40_49'];
-        $ages['50-55'] = $resultArray[0]['50_55'];
-        $ages['>55'] = $resultArray[0]['peste_55'];
+        $ages['<25'] = $resultArray['sub_25'];
+        $ages['25-29'] = $resultArray['25_29'];
+        $ages['30-39'] = $resultArray['30_39'];
+        $ages['40-49'] = $resultArray['40_49'];
+        $ages['50-55'] = $resultArray['50_55'];
+        $ages['>55'] = $resultArray['peste_55'];
         $toAdd['age'] = $ages;
     }
 
     return $toAdd;
 }
 
-function jsonYearFormat($resultArray,$categs,$start,$finish){
+function jsonYearFormat($resultArray, $categs, $start, $finish)
+{
     $months = [
         1 => 'january',
         2 => 'february',
@@ -99,15 +100,18 @@ function jsonYearFormat($resultArray,$categs,$start,$finish){
         9 => 'september',
         10 => 'october',
         11 => 'november',
-        12 => 'december',
+        12 => 'december'
     ];
     $toAdd = [];
-    for($i=$start;$i<=$finish;$i++){
+    $parity = -1;
+    for ($i = $start; $i <= $finish; $i++) {
+
         $month = [];
-        $month[$months[$i]] = $months[$i];
-        array_push($month, jsonFormat($resultArray[$i], $categs));
-        array_push($toAdd,$month);
+        array_push($month, jsonFormat($resultArray[$i-1], $categs));
+        $toAdd[$months[$i]] = $month;
+
     }
+    return $toAdd;
 }
 
 function queryDatabaseForCounty($params, $queryParams, $body, $headers)
@@ -137,7 +141,7 @@ function queryDatabaseForCounty($params, $queryParams, $body, $headers)
 
 
     $jsonFinal = array();
-    array_push($jsonFinal, jsonFormat($resultArray, $categs));
+    array_push($jsonFinal, jsonFormat($resultArray[0], $categs));
 
     print_r(json_encode($jsonFinal));
 }
@@ -166,14 +170,16 @@ function queryDatabase($params, $queryParams, $body, $headers)
         $query->setEndYearAndMonth($wantedYear, 2);
         $resultArray = $query->executeQuery();
         $jsonFinal = array();
-        array_push($jsonFinal, jsonFormat($resultArray, $categs));
+        array_push($jsonFinal, jsonYearFormat($resultArray, $categs,1,2));
     } else {
         $query->setStartYearAndMonth($wantedYear, 1);
         $query->setEndYearAndMonth($wantedYear, 12);
         $resultArray = $query->executeQuery();
-        print_r(json_encode($query->executeQuery()));
+        // print_r(json_encode($resultArray[0]));
         $jsonFinal = array();
-        array_push($jsonFinal, jsonFormat($resultArray, $categs));
-        // print_r(json_encode($jsonFinal));
+        array_push($jsonFinal, jsonYearFormat($resultArray, $categs, 1, 12));
     }
+
+    print_r(json_encode($jsonFinal));
+
 }
