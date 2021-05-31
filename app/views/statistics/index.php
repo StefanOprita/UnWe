c1f1f2694c3e722c2c1e8595c68e06598d563ae4
LiviuC <62299917+LiviuC-glitch@users.noreply.github.com>
2021-05-31 12:29:01 +0300

    js solved

--- app/views/statistics/index.php
+++ app/views/statistics/index.php
@@ -6,26 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UnWe</title>
-
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <link rel="stylesheet" href="../../../public/style.css">
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
-    <script src="../../../public/js/info.js"></script>
-    <script src="../../../public/js/script-statistics.js"></script>
-    <script src="../../../public/js/dot.js"></script>
-    <script src="../../../public/js/box.js"></script>
-    <script src="../../../public/js/animation.js"></script>
-    <script src="../../../lab/my-line-chart.js"></script>
-    <script src="../../../lab/my-bar-chart.js"></script>
-    <script src="../../../lab/my-pie-chart.js"></script>
-
-    <script src="https://jsuites.net/v4/jsuites.js"></script>
-    <link rel="stylesheet" href="https://jsuites.net/v4/jsuites.css" type="text/css" />
 </head>
 
 <body>
@@ -130,11 +110,27 @@
             </div>
 
         </div>
-
-
-
-
     </div>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <link rel="stylesheet" href="../../../public/style.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
+    <script src="../../../public/js/info.js"></script>
+    <script src="../../../public/js/script-statistics.js"></script>
+    <script src="../../../public/js/dot.js"></script>
+    <script src="../../../public/js/box.js"></script>
+    <script src="../../../public/js/animation.js"></script>
+    <script src="../../../lab/my-line-chart.js"></script>
+    <script src="../../../lab/my-bar-chart.js"></script>
+    <script src="../../../lab/my-pie-chart.js"></script>
+
+    <script src="https://jsuites.net/v4/jsuites.js"></script>
+    <link rel="stylesheet" href="https://jsuites.net/v4/jsuites.css" type="text/css" />
 </body>
 
 </html>
