552ce48592b55939c18b94a5cbf399461fd87707
LiviuC <62299917+LiviuC-glitch@users.noreply.github.com>
2021-06-09 00:35:36 +0300

    addEntry

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -20,28 +20,28 @@
         <div class="admin-container">
             <label class="add collision-box"><b>Insert</b></label>
             <form class="add-form" onsubmit="addEntry(); return false;">
-                <input class="collision-box" type="number" step="1" min="1970" max="2999" placeholder="Year" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="1" max="12" placeholder="Month" name="uname" required>
+                <input class="collision-box" type="number" step="1" min="1970" max="2999" placeholder="Year" name="uname" required id='add-year'>
+                <input class="collision-box" type="number" step="1" min="1" max="12" placeholder="Month" name="uname" required id='add-month'>
                 <input class="collision-box" type="text" placeholder="County" name="uname" required id='county'>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="< 25" name="uname" required >
-                <input class="collision-box" type="number" step="1" min="0" placeholder="25 - 29" name="uname" required >
-                <input class="collision-box" type="number" step="1" min="0" placeholder="30 - 39" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="40 - 49" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="50 - 55" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="55 >" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="Male" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="Female" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="NoEdu" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="Primary" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="Middle" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="High" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="Bachelors" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="Post" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="Professional" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="Urban" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="Rural" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="Comp" name="uname" required>
-                <input class="collision-box" type="number" step="1" min="0" placeholder="Not comp" name="uname" required>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="< 25" name="uname" required id='sub25'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="25 - 29" name="uname" required id='25to29'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="30 - 39" name="uname" required id='30to39'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="40 - 49" name="uname" required id='40to49'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="50 - 55" name="uname" required id='50to55'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="55 >" name="uname" required id='over55'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="Male" name="uname" required id='add-male'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="Female" name="uname" required id='add-female'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="NoEdu" name="uname" required id='noedu'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="Primary" name="uname" required id='primary'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="Middle" name="uname" required id='middle'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="High" name="uname" required id='high'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="Bachelors" name="uname" required id='bachelors'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="Post" name="uname" required id='post'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="Professional" name="uname" required id='prof'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="Urban" name="uname" required id='add-urban'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="Rural" name="uname" required id='add-rural'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="Comp" name="uname" required id='add-comp'>
+                <input class="collision-box" type="number" step="1" min="0" placeholder="Not comp" name="uname" required id='not-comp'>
 
                 <div class="break"></div>
 
